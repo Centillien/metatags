da774605d220db634dab40dec878dec380e29ef7
Gerard Kanters <gkanters@gmail.com>
2017-01-30 10:09:28 +0100

    Fix group icons

--- start.php
+++ start.php
@@ -23,7 +23,6 @@
 		$cloudflare = elgg_get_plugin_setting("cloudflare","metatags");
 	        if($cloudflare == "yes") {
 			elgg_register_plugin_hook_handler('entity:icon:url', 'user', 'metatags_user_icon_url_override');
-			elgg_register_plugin_hook_handler('entity:icon:url', 'group', 'metatags_group_icon_url_override');
 		}
 		
 		//Unregister systemlog since it is not very usefull	 
@@ -45,27 +44,6 @@ function metatags_user_icon_url_override($hook, $type, $returnvalue, $params) {
                return "_graphics/icons/user/default{$size}.gif";
           }
      }
-}
-
-function metatags_group_icon_url_override($hook, $type, $returnvalue, $params) {
-
-	$group = $params['entity'];
-	$size = $params['size'];
-
-	$icontime = $group->icontime;
-	if (null === $icontime) {
-		$file = new ElggFile();
-		$file->owner_guid = $group->owner_guid;
-		$file->setFilename("groups/" . $group->guid . "large.jpg");
-		$icontime = $file->exists() ? time() : 0;
-		create_metadata($group->guid, 'icontime', $icontime, 'integer', $group->owner_guid, ACCESS_PUBLIC);
-        }
-	if ($icontime) {
-		// return thumbnail
-		return "groupicon/$group->guid/$size/$group->name.jpg";
-	}
-
-	return "mod/groups/graphics/default{$size}.gif";
 }
 
 elgg_register_event_handler('init','system','metatagsgen_init');
